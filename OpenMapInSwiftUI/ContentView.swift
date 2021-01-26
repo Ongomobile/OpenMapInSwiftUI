0196a5a8cb341661ef745da99074aa37f18f340f
Michael Haslam <ongomobile@gmail.com>
2021-01-26 15:08:11 -0800

    Initial Commit

---
+++ OpenMapInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpenMapInSwiftUI
+//
+//  Created by Michael Haslam on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpenMapInSwiftUI/OpenMapInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMapInSwiftUIApp.swift
+//  OpenMapInSwiftUI
+//
+//  Created by Michael Haslam on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct OpenMapInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
